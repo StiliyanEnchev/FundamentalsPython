f82d22cbea29dfbbf725d99e1cf61195fd0fd218
StiliyanEnchev <stillg8@gmail.com>
2023-09-27 15:02:26 +0300

    Programming Fundamentals with Python - September 2023 update

---
+++ data_types_and_variables_lab/special_numbers.py
@@ -0,0 +1,6 @@
+integer = int(input())
+special_number = False
+for num in range(1, integer + 1):
+    sum_of_digits = 0
+    digits = num
+    
--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-n = int(input())
-for i in range(1, n + )1:
-    print(' ' * (n - i) + '* ' * i)
+name = input()
+print(name[::-2])
