3f5b86e9b9a4c4cddd9229e1e04ec15c568bf326
StiliyanEnchev <stillg8@gmail.com>
2023-09-28 09:39:21 +0300

    Programming Fundamentals with Python - September 2023 update

--- data_types_and_variables_lab/special_numbers.py
+++ data_types_and_variables_lab/special_numbers.py
@@ -1,6 +1,10 @@
 integer = int(input())
-special_number = False
 for num in range(1, integer + 1):
+    current_num = str(num)
     sum_of_digits = 0
-    digits = num
-    
+    for current_dig in current_num:
+        sum_of_digits += int(current_dig)
+    if sum_of_digits == 7 or sum_of_digits == 5 or sum_of_digits == 11:
+        print(f"{num} -> True")
+    else:
+        print(f'{num} -> False')
