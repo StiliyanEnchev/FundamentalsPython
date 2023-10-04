0619c2c6220470e235f79c0da1981c78d4ba9998
StiliyanEnchev <stillg8@gmail.com>
2023-10-04 18:31:01 +0300

    Programming Fundamentals with Python - September 2023 update

--- lists_basics_exer/seize_the_fire.py
+++ lists_basics_exer/seize_the_fire.py
@@ -2,7 +2,7 @@ all_fire_cells = input().split('#')
 left_water = int(input())
 effort_counter = 0
 total_water = left_water
-
+filtered_list = []
 for cell_number in range(len(all_fire_cells)):
     current_cell_fire_details = (all_fire_cells[cell_number]).split(" = ")
     fire_type, fire_volume = current_cell_fire_details[0], int(current_cell_fire_details[1])
@@ -11,12 +11,14 @@ for cell_number in range(len(all_fire_cells)):
         if left_water >= fire_volume and 81 <= fire_volume <= 125:
             left_water -= fire_volume
             effort_counter += fire_volume * 0.25
+            filtered_list.append(fire_volume)
         else:
             all_fire_cells[cell_number] = None
     elif fire_type == 'Medium':
         if left_water >= fire_volume and 51 <= fire_volume <= 80:
             left_water -= fire_volume
             effort_counter += fire_volume * 0.25
+            filtered_list.append(fire_volume)
         else:
             all_fire_cells[cell_number] = None
 
@@ -24,11 +26,13 @@ for cell_number in range(len(all_fire_cells)):
         if left_water >= fire_volume and 1 <= fire_volume <= 50:
             left_water -= fire_volume
             effort_counter += fire_volume * 0.25
+            filtered_list.append(fire_volume)
         else:
             all_fire_cells[cell_number] = None
 
 total_putted_fire = total_water - left_water
-
 print(f'Cells:')
+for cell in range(len(filtered_list)):
+    print(f' - {filtered_list[cell]}')
 print(f'Effort: {effort_counter:.2f}')
 print(f'Total Fire: {total_putted_fire}')
