193bf5c0334a0c7c24cc2a7305e41d725c06ae78
StiliyanEnchev <stillg8@gmail.com>
2023-10-04 13:21:56 +0300

    Programming Fundamentals with Python - September 2023 update

--- lists_basics_exer/easter_gifts.py
+++ lists_basics_exer/easter_gifts.py
@@ -1,5 +1,20 @@
 bought_gifts = input().split()
 command = input()
-
 while command != "No Money":
-    if command ==
+    command_and_gift = command.split()
+    new_command = command_and_gift[0]
+    gift_name = command_and_gift[1]
+    if new_command == "OutOfStock":
+        for gift in range(len(bought_gifts)):
+            if gift_name == bought_gifts[gift]:
+                bought_gifts[gift] = None
+    elif new_command == "Required":
+        index = int(command_and_gift[2])
+        if index < (len(bought_gifts)):
+            bought_gifts[index] = gift_name
+    elif new_command == "JustInCase":
+        bought_gifts[-1] = gift_name
+    command = input()
+for gifts in range(len(bought_gifts)):
+    if bought_gifts[gifts] is not None:
+        print(bought_gifts[gifts], end=" ")
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-integers_as_strings = input().split()
-remove_count = int(input())
-new_integer_list = list(map(int, integers_as_strings))
+bought_gifts = input().split()
+command = input()
 
-for num in range(remove_count):
-    min_num = min(new_integer_list)
-    new_integer_list.remove(min_num)
-
-for index in range(len(integers_as_strings)):
-    current_number = int(integers_as_strings[index])
-    print(current_number, end=', ')
+while command != "No Money":
+    command_and_gift = command.split()
+    new_command = command_and_gift[0]
+    gift_name = command_and_gift[1]
+    if new_command == "OutOfStock":
+        for gift in range(len(bought_gifts) - 1, -1, -1):
+            if gift_name == bought_gifts[gift]:
+                bought_gifts.remove(gift_name)
+    elif new_command == "Required":
+        given_index = int(command_and_gift[2])
+        print(given_index)
+        command = input()
